9ee8d3522de733f34a4fcec8c4857b03a16a261a
Arbin <itzarbin@gmail.com>
2025-05-04 17:08:39 +0545

    Commited Changes:

--- resources/views/profile/partials/navbar.blade.php
+++ resources/views/profile/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="bg-white shadow-md px-6 py-4 flex justify-between items-center">
     <div>
-        <img src="{{ asset('media/evenova_1.png') }}" alt="Evenova Logo" class="h-8 w-8 mr-2"> 
+        <img src="{{ asset('media/evenova_1.png') }}" alt="Evenova Logo" class="h-8 w-10 "> 
     </div>
 
     <ul class="hidden md:flex space-x-6">
@@ -13,3 +13,5 @@
         <a href="{{ url('/register') }}" class="px-4 py-2 rounded-md bg-gradient-to-r from-blue-500 to-purple-600 text-white">Sign Up</a>
     </div>
 </nav>
+
+
