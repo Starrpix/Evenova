750aef43fba5e633b0bce5bab6ec0883809b1264
Arbin <itzarbin@gmail.com>
2025-05-04 13:12:43 +0545

    Initial Commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,7 @@
+<body>
+    @include('profile.partials.navbar')  
+    <main>
+        @yield('content')        
+    </main>
+    @include('profile.partials.footer')  
+</body>
---
+++ resources/views/profile/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>&copy; {{ date('Y') }} Evenova. All rights reserved.</p>
+</footer>
